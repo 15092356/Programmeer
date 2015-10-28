69b2648780ef021c8de2af44cfd54ba1d9ed1215
Wouter <Wouter@Wouters-PC.wireless.hhs.nl>
2015-10-28 15:23:36 +0100

    NEW GUI

---
+++ src/programmeer/Programmeer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programmeer;
+
+/**
+ *
+ * @author Wouter
+ */
+public class Programmeer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
